12d472e4a4b6aee87d196d38dd83ef44688e4934
sebastian461 <sebastiano-461@hotmail.com>
2023-12-30 01:23:12 -0500

    Diseño de registro
    Se creo un layout para los formularios cambiando el diseño del login y creando el diseño del registro

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,54 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const RegisterPage = () => {
-  return <h1>RegisterPage</h1>;
+  return (
+    <AuthLayout title="Registro">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre completo"
+              type="text"
+              placeholder="Sebastián Álava"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@ejemplo.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid>
+
+            <Grid container direction="row" justifyContent="end">
+              <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+              <Link component={RouterLink} color="inherit" to="/auth/login">
+                Ingresar
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
