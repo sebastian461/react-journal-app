a3a70fa4082bd9703624836229d4e437b98fa459
sebastian461 <sebastiano-461@hotmail.com>
2023-12-30 02:14:20 -0500

    Diseño de NavBar y SideBar

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,13 +1,8 @@
-import { MailOutline } from "@mui/icons-material";
-import { Typography } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
 export const JournalApp = () => {
   return (
     <>
-      <Typography variant="h1">JournalApp</Typography>
-      <MailOutline />
-
       <div id="detail">
         <Outlet />
       </div>
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,15 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import { JournalLayout } from "../layout/JournalLayout";
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam
+        molestiae, ipsa aspernatur obcaecati commodi tempore labore porro in,
+        sapiente fuga odit! Rerum ipsam fugiat natus, nemo aut laudantium
+        expedita optio.
+      </Typography>
+    </JournalLayout>
   );
 };
