c8c8123b6f82a2f6f01195bb1d902f0b4c30c3a2
sebastian461 <sebastiano-461@hotmail.com>
2024-01-11 23:51:35 -0500

    Firestore + newNote
    Se configuro Firestore de Firebase para generar la base de datos y se hizo la conexión para crear nuevas notas

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -3,12 +3,21 @@ import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 import { IconButton } from "@mui/material";
 import { AddOutlined } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { startNewNote } from "../../store/journal";
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
+
   return (
     <JournalLayout>
       <NothingSelectedView />
       {/* <NoteView /> */}
       <IconButton
+        onClick={onClickNewNote}
         size="large"
         sx={{
           color: "white",
---
+++ src/store/journal/thunks.js
@@ -0,0 +1,20 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../firebase/config";
+
+export const startNewNote = () => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
+    const newNote = {
+      titel: "",
+      body: "",
+      date: new Date().getTime(),
+    };
+
+    const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
+    await setDoc(newDoc, newNote);
+
+    //TODO
+    //dispatch(newNote)
+    //dispatch(activarNote)
+  };
+};
